2a6a48b89762adfa0073b15a2106a81a639d877e
thomas-marcoux <txmarcoux@ualr.edu>
2017-10-24 11:04:01 -0500

    Superficial revision

--- multilayerPerceptron.py
+++ multilayerPerceptron.py
@@ -6,9 +6,11 @@ import pandas as pd
 import numpy as np
 
 
+#Write predictions to file
 def write_preds(preds, fname):
         pd.DataFrame({"ImageId": list(range(1,len(preds)+1)), "Label": preds}).to_csv(fname, index=False, header=True)
 
+#Build Perceptron model from the input dimension and the number of classes/digits
 def build_model(input_dim, nb_classes):
     model = Sequential()
     model.add(Dense(128, input_dim=input_dim))
@@ -22,14 +24,14 @@ def build_model(input_dim, nb_classes):
     return model
 
 def DR_MLP():
-    # Read data
+    #Read data
     train = pd.read_csv('train.csv')
     labels = train.ix[:,0].values.astype('int32')
     X_train = (train.ix[:,1:].values).astype('float32')
     X_test = (pd.read_csv('test.csv').values).astype('float32')
-    # convert list of labels to binary class matrix
+    #Convert list of labels to binary class matrix
     y_train = np_utils.to_categorical(labels) 
-    # pre-processing: divide by max and substract mean
+    #Pre-processing: divide by max and substract mean
     scale = np.max(X_train)
     X_train /= scale
     X_test /= scale
@@ -38,16 +40,18 @@ def DR_MLP():
     X_test -= mean
     input_dim = X_train.shape[1]
     nb_classes = y_train.shape[1]
-    # Here's a Deep Dumb MLP (DDMLP)
+    
+    #Build and fit model
     model = build_model(input_dim, nb_classes)
-    # we'll use categorical xent for the loss, and RMSprop as the optimizer
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
     print("Training...")
     model.fit(X_train, y_train, epochs=10, batch_size=16, validation_split=0.1, verbose=2)
+    
+    #Evaluate model on training set, then write predictions for the testing set to file
     scores = model.evaluate(X_train, y_train, verbose=0)
     print("Baseline Error: %.2f%%" % (100-scores*100))
     print("Generating test predictions...")
     preds = model.predict_classes(X_test, verbose=0)
-    write_preds(preds, "keras-mlp.csv")
+    write_preds(preds, "mlp-results.csv")
 
 DR_MLP()
--- svm.py
+++ svm.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt, matplotlib.image as mpimg
+
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 
+#Display digits from the picture set, with their label
 def view_image(i, train_images, train_labels):
     img=train_images.iloc[i].as_matrix()
     img=img.reshape((28,28))
@@ -13,30 +15,33 @@ def view_image_pixel_value(n, train_images):
     plt.hist(train_images.iloc[n])
 
 def DR_SVC():
+    #Read and label data
     labeled_images = pd.read_csv('train.csv')
     images = labeled_images.iloc[0:5000,1:]
     labels = labeled_images.iloc[0:5000,:1]
+    
+    #Split data into training and testing sets for the SVM
     train_images, test_images,train_labels, test_labels = train_test_split(images, labels, train_size=0.8, random_state=0)
+    #Uncomment to view the training set:
     #view_image(n, train_images, train_labels)
-    #Load scikit-learn Support Vector Classifier
-    clf = svm.SVC()
-    clf.fit(train_images, train_labels.values.ravel())
-    print(clf.score(test_images,test_labels))
-    #For all pictures in container where pixel is grey, round up
+    
+    #Standardization: for all pictures in container where pixel is grey, round up
     test_images[test_images>0]=1
     train_images[train_images>0]=1
-    #img=train_images.iloc[n].as_matrix().reshape((28,28))
-    #This displays image histogram (black and white if previous operations succeeded)
-    #n = 6
-    #plt.imshow(img,cmap='binary')
-    #plt.title(train_labels.iloc[n])
-    #view_image_pixel_value(n, train_images)
+    
+    #Load scikit-learn Support Vector Classifier and fit the model
     clf = svm.SVC()
     clf.fit(train_images, train_labels.values.ravel())
+    
+    #Displays the accuracy of the model on the training set
     print(clf.score(test_images,test_labels))
+    
+    #Read testing set and generate predictions
     test_data=pd.read_csv('test.csv')
     test_data[test_data>0]=1
     results=clf.predict(test_data[0:5000])
+    
+    #Display sample predictions and write to file
     print(results[0:10])
     df = pd.DataFrame(results)
     df.index.name='ImageId'
